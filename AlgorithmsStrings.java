1cf2f9aaa0d08b9b9114f8f72d44d73f1c390449
DanielMoonK <danielk213@nycstudents.net>
2024-12-04 17:35:22 +0000

    first commit

---
+++ AlgorithmsStrings.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class AlgorithmsStrings {
+    private static File f;
+    private static Scanner s;
+    public static void main(String[] args) throws FileNotFoundException {
+        f = new File("words.txt");
+        int twoLetter = twoLetter();
+        int numLong = numLongest();
+        int numPal = numPalindromes();
+
+        System.out.print("Number of two letter words: ");
+        System.out.println(twoLetter);
+        System.out.print("Number of longest letter words: ");
+        System.out.println(numLong);
+        s.close();
+    }
+
+    public static int twoLetter() throws FileNotFoundException{
+        s = new Scanner(f);
+        int twoLetter = 0;
+        while(s.hasNext()){
+            String nextWord = s.nextLine();
+            if(nextWord.length() == 2){
+                twoLetter++;
+                // System.out.println(nextWord);
+            }
+        }
+        return twoLetter;
+    }
+
+    public static int numLongest() throws FileNotFoundException{
+        s = new Scanner(f);
+        int count = 0;
+        int longLength = 0;
+        while(s.hasNext()){
+            String nextWord = s.nextLine();
+            int length = nextWord.length();
+            if(length > longLength){
+                count = 0;
+                longLength = length;
+                //System.out.println(longLength);
+            }
+            if(length == longLength){
+                count++;
+                //System.out.println(nextWord);
+            }
+        }
+        return count;
+    }
+
+    public static int numPalindromes() throws FileNotFoundException{
+        s = new Scanner(f);
+        int numPal = 0;
+        while(s.hasNext()){
+            String nextWord = s.nextLine();
+            if(nextWord.length() % 2 == 0){
+                String firstHalf = nextWord.substring(0, nextWord.length()/2);
+                String secondHalf = "";
+                for(int i=0;i<nextWord.length()/2;i++){
+                    secondHalf += nextWord.substring(nextWord.length()-i, nextWord.length()-i-1);
+                }
+            }
+        }
+    }
+}
